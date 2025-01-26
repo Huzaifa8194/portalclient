01d765e79d9cf5bdd0f3a9dcb77887d63fa6da2a
Syed-AliH <ali.hassan0023@outlook.com>
2025-01-26 21:38:56 +0500

    Routing of EOR & Payroll

--- src/pages/dashboard/EOR/new.jsx
+++ src/pages/dashboard/EOR/new.jsx
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet-async';
 
 import { CONFIG } from 'src/config-global';
 
-import { PostCreateView } from 'src/sections/entrepenaur/view';
+import { PostCreateView } from 'src/sections/EOR/view';
 
 // ----------------------------------------------------------------------
 
